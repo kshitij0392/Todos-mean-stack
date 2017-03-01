5ab3b11013940837e6d6d34ee52c7047eb3d8f90
Kshitij Rangari <Kshitij Rangari>
2017-03-01 15:02:00 -0500

    changes in client side setup

--- routes/todos.js
+++ routes/todos.js
@@ -1,102 +1,93 @@
 var express = require('express');
 var router = express.Router();
-var mongojs = require ('mongojs');
-var db = mongojs('mongodb://kshitu:admin@ds157529.mlab.com:57529/meantodos_kshitu_dev',['todos']);
-
+var mongojs = require('mongojs');
+var db = mongojs('mongodb://admin:admin@ds041516.mlab.com:41516/meantodos_brad_dev',['todos']);
 
 // Get Todos
 router.get('/todos', function(req, res, next){
-    db.todos.find (function (err, todos){
-            if (err){
-                res.send(err);
-            } else {
-                res.json (todos);
-            }
+    db.todos.find(function(err, todos){
+        if(err){
+           res.send(err); 
+        } else {
+           res.json(todos);
+        }
     });
 });
 
-// Get Single Todos
+// Get Single Todo
 router.get('/todo/:id', function(req, res, next){
-    db.todos.findOne ({
-        _id:mongojs.ObjectId(req.params.id)
-     }, function (err, todo){
-            if (err){
-                res.send(err);
-            } else {
-                res.json (todo);
-            }
+    db.todos.findOne({
+        _id: mongojs.ObjectId(req.params.id)
+    }, function(err, todo){
+        if(err){
+           res.send(err); 
+        } else {
+           res.json(todo);
+        }
     });
 });
 
-// Save todo
-
-router.post ('/todo', function (req,res,next){
+// Save Todo
+router.post('/todo', function(req, res, next){
     var todo = req.body;
-    if (!todo.text || !(todo.isCompleted+ '')){
+    if(!todo.text || !(todo.isCompleted + '')){
         res.status(400);
-        res.json ({
-            "Error" : "Invalid Data"
+        res.json({
+            "error": "Invalid Data"
         });
     } else {
-        db.save (todo, function(err,result){
-            if (err){
-                res.send(err);
+        db.save(todo, function(err, result){
+            if(err){
+                res.send(err); 
             } else {
-                res.json (result);
+                res.json(result);
             }
-        
-        } );
+        });
     }
 });
 
-// Update Todos
-
-router.put('/todo/:id', function (req,res,next){
+// Update Todo
+router.put('/todo/:id', function(req, res, next){
     var todo = req.body;
     var updObj = {};
-    if (todo.isCompleted){
-        updObj.isCompleted = todo.isCompleted;
+    
+    if(todo.isCompleted){
+       updObj.isCompleted = todo.isCompleted;
     }
-
-    if (todo.text){
-        updobj.text = todo.text
+    
+    if(todo.text){
+        updObj.text = todo.text;
     }
-    if (!updObj ){
-        res.status (400);
+    
+    if(!updObj){
+        res.status(400);
         res.json({
-            "error":"Invalid Data"
+            "error": "Invalid Data"
         });
-    } 
-        else {
-            db.todos.update({
-                _id : mongojs.ObjectId(req.param.id)
-        }, updObj, {}, function (err,result){
-             if (err){
-                res.send(err);
+    } else {
+        db.todos.update({
+            _id: mongojs.ObjectId(req.params.id)
+        },updObj, {}, function(err, result){
+            if(err){
+                res.send(err); 
             } else {
-                res.json (result);
+                res.json(result);
             }
         });
     }
-
-    
 });
 
-// Delete Todo 
-
-
-router.delete( '/todo/:id', function (req,res,next){
-     db.todos.remove({
-                _id : mongojs.ObjectId(req.param.id)
-        }, '', function (err,result){
-             if (err){
-                res.send(err);
-            } else {
-                res.json (result);
-            }
-        });
+// Delete Todo
+router.delete('/todo/:id', function(req, res, next){
+    db.todos.remove({
+        _id: mongojs.ObjectId(req.params.id)
+    },'', function(err, result){
+        if(err){
+            res.send(err); 
+        } else {
+            res.json(result);
+        }
     });
-
-
+});
 
 module.exports = router;
